a4ff1b9a0316958523b0f817545208fea28629bf
yuchen <yuchen@holoi.com>
2022-03-29 18:03:53 +0800

    Initial Commit

---
+++ any-workout-project/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  any-workout-project
+//
+//  Created by Yuchen Zhang on 2022/3/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ any-workout-project/any_workout_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  any_workout_projectApp.swift
+//  any-workout-project
+//
+//  Created by Yuchen Zhang on 2022/3/29.
+//
+
+import SwiftUI
+
+@main
+struct any_workout_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
