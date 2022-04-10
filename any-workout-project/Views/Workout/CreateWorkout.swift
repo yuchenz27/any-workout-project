ba668add8e6c726787e2c7759813a1cc3b6b0e75
yuchen <yuchen@holoi.com>
2022-04-10 14:29:43 +0800

    Added TextFieldInput and Picker

--- any-workout-project/Views/Workout/CreateWorkout.swift
+++ any-workout-project/Views/Workout/CreateWorkout.swift
@@ -10,8 +10,7 @@ import SwiftUI
 // Inputfield https://www.youtube.com/watch?v=-_-BNwUZrrc
 struct CreateWorkout: View {
     
-    @State private var showingPopover = false
-    @State private var newExerciseName: String = "New Exercise"
+    @State private var showingSheet = false
     
     var exercises: [Exercise]?
     
@@ -23,16 +22,13 @@ struct CreateWorkout: View {
             ExerciseRow()
             
             Button {
-                showingPopover = true
+                showingSheet = true
             } label: {
                 Label("Add Exercise", systemImage: "plus.circle")
                     .labelStyle(.iconOnly)
             }
-            .popover(isPresented: $showingPopover) {
-                VStack {
-                    Text("New Exercise")
-                    TextField("New Exercise Name", text: $newExerciseName)
-                }
+            .sheet(isPresented: $showingSheet) {
+                ExerciseDetail()
             }
         }
         .navigationTitle("Create Workout")
@@ -40,8 +36,65 @@ struct CreateWorkout: View {
     }
 }
 
+struct ExerciseDetail: View {
+    
+    @Environment(\.presentationMode) var presentationMode
+    // presentationMode.wrappedValue.dismiss()
+    
+    @State var exerciseName: String = ""
+    @State var hours: Int = 0
+    @State var minutes: Int = 0
+    @State var seconds: Int = 0
+    var exercise: Exercise?
+    
+    var body: some View {
+        ZStack {
+            Color.pink
+            
+            VStack {
+                Text("Exercise Name")
+                TextField("Type the name here...", text: $exerciseName)
+                    .padding()
+                    .background(Color.gray.opacity(0.3).cornerRadius(10))
+                    .foregroundColor(.white)
+                    .font(.headline)
+                
+                Text("Exercise Duration")
+                HStack {
+                    // Hours
+                    Picker(selection: $hours, label: Text("Hours")) {
+                        ForEach(0..<25) { num in
+                            Text("\(num)").tag(num)
+                        }
+                    }
+                    .pickerStyle(.wheel)
+//                    // Minutes
+//                    Picker(selection: $minutes, label: Text("Minutes")) {
+//                        ForEach(0..<60) { num in
+//                            Text("\(num)").tag(num)
+//                        }
+//                    }
+//                    .pickerStyle(.wheel)
+//                    // Seconds
+//                    Picker(selection: $seconds, label: Text("Seconds")) {
+//                        ForEach(0..<60) { num in
+//                            Text("\(num)").tag(num)
+//                        }
+//                    }
+//                    .pickerStyle(.wheel)
+                }
+                
+                
+
+            }
+            .padding()
+        }
+    }
+}
+
 struct CreateWorkout_Previews: PreviewProvider {
     static var previews: some View {
-        CreateWorkout()
+        //CreateWorkout()
+        ExerciseDetail()
     }
 }
